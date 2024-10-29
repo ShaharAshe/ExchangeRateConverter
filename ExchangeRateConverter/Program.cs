d337bff9d14a39b10b90ed2fcf4adf3780e78720
ShaharAshe <shaharas30@gmail.com>
2024-10-29 13:53:40 +0200

    Add singleton class for Utilities and started handling the files.

--- ExchangeRateConverter/Program.cs
+++ ExchangeRateConverter/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace ExchangeRateConverter
 {
@@ -10,7 +12,30 @@ namespace ExchangeRateConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World {0} {1}", args[0], args.Length);
+            var utilities = Utilities.Instance;
+            Console.WriteLine("Singleton Value: " + utilities.FILE_NAME);
+
+            try
+            {
+                Console.WriteLine("Hello World {0} {1}\n", args[utilities.FILE_NAME], args.Length);
+                string[] lines = File.ReadAllLines(args[utilities.FILE_NAME])
+                    .Where(line => !string.IsNullOrWhiteSpace(line))
+                    .Select(line => line.Trim())
+                    .ToArray();
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.Error.WriteLine("[ERROR] - You need to insert a file name in the arguments vector !!!");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
             Console.ReadLine();
         }
     }
---
+++ ExchangeRateConverter/utilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExchangeRateConverter
+{
+    public sealed class Utilities
+    {
+        private static Utilities _instance = null;
+        private static readonly object _lock = new object();
+        private Utilities()
+        {
+            FILE_NAME = 0;
+
+            FROM_COUNTRY = 0;
+            TO_COUNTRY = 1;
+        }
+        public static Utilities Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Utilities();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public readonly int FILE_NAME;
+
+        public readonly int FROM_COUNTRY;
+        public readonly int TO_COUNTRY;
+    }
+}
